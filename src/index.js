42b4e0491b04026089f93226b03a47353b4219d3
Junedkhan <junedkhanpathan95@gmail.com>
2023-11-22 15:36:26 +0530

    bug fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import mongoose from "mongoose";
 // import {DB_NAME} from './constants.js'
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
 
@@ -8,8 +9,26 @@ dotenv.config({
 })
 
 
-connectDB()
 
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is running on port : ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log(`MongoDB Connection failed`, err)
+    })
+
+
+
+
+
+
+
+
+
+    
 /*
 import express from 'express'
 const app = express()
